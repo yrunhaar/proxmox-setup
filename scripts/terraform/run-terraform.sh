84ced9200e5a80698ff115b8091e266df9c4f4f3
Yohan Runhaar <yohanrunhaar@Yohans-MacBook-Pro.local>
2024-11-01 18:10:54 +0100

    fix run terra script

--- scripts/terraform/run-terraform.sh
+++ scripts/terraform/run-terraform.sh
@@ -8,26 +8,30 @@ cyan() { echo -e "\033[36m$1\033[0m"; }
 
 # Define paths
 PROXMOX_SETUP_DIR="$HOME/proxmox-setup"
-PACKER_DIR="$PROXMOX_SETUP_DIR/packer"
+PACKER_DIR="$PROXMOX_SETUP_DIR/packer/talos-packer"
+PACKER_VAR_FILE="$PACKER_DIR/vars/local.pkrvars.hcl"
 TERRAFORM_DIR="$PROXMOX_SETUP_DIR/terraform"
+TF_PLAN_FILE="$TERRAFORM_DIR/.tfplan"
 
 # Function to build Packer image
 build_packer() {
     blue "Building Packer image..."
-    cd "$PACKER_DIR/talos-packer" || { red "Failed to navigate to $PACKER_DIR/talos-packer"; exit 1; }
-    packer init -upgrade . || { red "Packer initialization failed"; exit 1; }
-    packer validate -var-file="vars/local.pkrvars.hcl" . || { red "Packer validation failed"; exit 1; }
-    packer build -var-file="vars/local.pkrvars.hcl" . || { red "Packer build failed"; exit 1; }
+    
+    packer init -upgrade "$PACKER_DIR" || { red "Packer initialization failed"; exit 1; }
+    packer validate -var-file="$PACKER_VAR_FILE" "$PACKER_DIR" || { red "Packer validation failed"; exit 1; }
+    packer build -var-file="$PACKER_VAR_FILE" "$PACKER_DIR" || { red "Packer build failed"; exit 1; }
+    
     green "Packer image built successfully!"
 }
 
 # Function to build Terraform configuration
 build_terraform() {
     blue "Building Terraform configuration..."
-    cd "$TERRAFORM_DIR" || { red "Failed to navigate to $TERRAFORM_DIR"; exit 1; }
-    terraform init || { red "Terraform initialization failed"; exit 1; }
-    terraform plan -out .tfplan || { red "Terraform plan failed"; exit 1; }
-    terraform apply .tfplan || { red "Terraform apply failed"; exit 1; }
+    
+    terraform -chdir="$TERRAFORM_DIR" init || { red "Terraform initialization failed"; exit 1; }
+    terraform -chdir="$TERRAFORM_DIR" plan -out="$TF_PLAN_FILE" || { red "Terraform plan failed"; exit 1; }
+    terraform -chdir="$TERRAFORM_DIR" apply "$TF_PLAN_FILE" || { red "Terraform apply failed"; exit 1; }
+    
     green "Terraform configuration applied successfully!"
 }
 
