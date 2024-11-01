9df0b98e5780148ee181cd0a274dab19684222df
Yohan Runhaar <yohanrunhaar@Yohans-MacBook-Pro.local>
2024-11-01 18:07:17 +0100

    fix run terra script

--- scripts/terraform/run-terraform.sh
+++ scripts/terraform/run-terraform.sh
@@ -6,32 +6,28 @@ green() { echo -e "\033[32m$1\033[0m"; }
 blue() { echo -e "\033[34m$1\033[0m"; }
 cyan() { echo -e "\033[36m$1\033[0m"; }
 
-
 # Define paths
 PROXMOX_SETUP_DIR="$HOME/proxmox-setup"
 PACKER_DIR="$PROXMOX_SETUP_DIR/packer"
 TERRAFORM_DIR="$PROXMOX_SETUP_DIR/terraform"
 
-# Function to provide Proxmox VM setup instructions
+# Function to build Packer image
 build_packer() {
     blue "Building Packer image..."
-    cd $PACKER_DIR/talos-packer/
-    packer init -upgrade .
-    packer validate -var-file="vars/local.pkrvars.hcl" .
-    packer build -var-file="vars/local.pkrvars.hcl" .
+    cd "$PACKER_DIR/talos-packer" || { red "Failed to navigate to $PACKER_DIR/talos-packer"; exit 1; }
+    packer init -upgrade . || { red "Packer initialization failed"; exit 1; }
+    packer validate -var-file="vars/local.pkrvars.hcl" . || { red "Packer validation failed"; exit 1; }
+    packer build -var-file="vars/local.pkrvars.hcl" . || { red "Packer build failed"; exit 1; }
     green "Packer image built successfully!"
 }
 
-# Function to build Terraform
+# Function to build Terraform configuration
 build_terraform() {
     blue "Building Terraform configuration..."
-    cd $TERRAFORM_DIR/
-    # Initialize Terraform
-    terraform init
-    # Plan
-    terraform plan -out .tfplan
-    # Apply
-    terraform apply .tfplan
+    cd "$TERRAFORM_DIR" || { red "Failed to navigate to $TERRAFORM_DIR"; exit 1; }
+    terraform init || { red "Terraform initialization failed"; exit 1; }
+    terraform plan -out .tfplan || { red "Terraform plan failed"; exit 1; }
+    terraform apply .tfplan || { red "Terraform apply failed"; exit 1; }
     green "Terraform configuration applied successfully!"
 }
 
